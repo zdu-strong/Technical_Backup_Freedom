8ee8226d29624fe1c8ae97fb28fcc63c7c224525
zdu <zdu.strong@gmail.com>
2024-01-08 10:58:34 +0900

    #: Fixed bug of incorrect number of descendants

--- springboot/src/main/java/com/springboot/project/common/database/CustomCockroachdbDialect.java
+++ springboot/src/main/java/com/springboot/project/common/database/CustomCockroachdbDialect.java
@@ -1,59 +1,52 @@
 package com.springboot.project.common.database;
 
 import org.hibernate.boot.model.FunctionContributions;
-import org.hibernate.dialect.DatabaseVersion;
-import org.hibernate.dialect.MySQLDialect;
-import org.hibernate.dialect.MySQLServerConfiguration;
 import org.hibernate.dialect.function.StandardSQLFunction;
 import org.hibernate.engine.jdbc.dialect.spi.DialectResolutionInfo;
 import org.hibernate.query.sqm.function.SqmFunctionRegistry;
 import org.hibernate.type.BasicTypeRegistry;
 import org.hibernate.type.StandardBasicTypes;
+import org.hibernate.dialect.CockroachDialect;
+import org.hibernate.dialect.DatabaseVersion;
+import org.hibernate.dialect.PostgreSQLDriverKind;
 
-/**
- * In order to use the ifnull method when selecting. In order to use the
- * found_rows method to get the total number of items in group by.
- * 
- * @author zdu
- *
- */
-public class CustomMySQLDialect extends MySQLDialect {
+public class CustomCockroachdbDialect extends CockroachDialect {
 
     @Override
     public void initializeFunctionRegistry(FunctionContributions functionContributions) {
         super.initializeFunctionRegistry(functionContributions);
         BasicTypeRegistry basicTypeRegistry = functionContributions.getTypeConfiguration().getBasicTypeRegistry();
         SqmFunctionRegistry functionRegistry = functionContributions.getFunctionRegistry();
-        functionRegistry.register("IFNULL", new StandardSQLFunction("IFNULL", StandardBasicTypes.LONG));
+        functionRegistry.register("IFNULL", new StandardSQLFunction("COALESCE", StandardBasicTypes.LONG));
         functionRegistry.registerPattern("FOUND_ROWS", "COUNT(*) OVER()",
                 basicTypeRegistry.resolve(StandardBasicTypes.LONG));
         functionRegistry.registerPattern("IS_SORT_AT_BEFORE", "?1 < ?2",
                 basicTypeRegistry.resolve(StandardBasicTypes.BOOLEAN));
-        functionRegistry.registerPattern("LOCATE", "LOCATE(?2, ?1)",
+        functionRegistry.registerPattern("LOCATE", "POSITION(?2 IN ?1)",
                 basicTypeRegistry.resolve(StandardBasicTypes.LONG));
         functionRegistry.registerPattern("FORMAT_DATE_AS_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND_MILLISECOND",
-                "SUBSTRING(DATE_FORMAT(CONVERT_TZ(?1, '+00:00', ?2), '%Y-%m-%d %H:%i:%s.%f'), 1, 23)",
+                "to_char(?1 AT TIME ZONE ?2, 'YYYY-MM-DD HH24:MI:SS.MS')",
                 basicTypeRegistry.resolve(StandardBasicTypes.STRING));
         functionRegistry.registerPattern("FORMAT_DATE_AS_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND",
-                "DATE_FORMAT(CONVERT_TZ(?1, '+00:00', ?2), '%Y-%m-%d %H:%i:%s')",
+                "to_char(?1 AT TIME ZONE ?2, 'YYYY-MM-DD HH24:MI:SS')",
                 basicTypeRegistry.resolve(StandardBasicTypes.STRING));
         functionRegistry.registerPattern("FORMAT_DATE_AS_YEAR_MONTH_DAY_HOUR_MINUTE",
-                "DATE_FORMAT(CONVERT_TZ(?1, '+00:00', ?2), '%Y-%m-%d %H:%i')",
+                "to_char(?1 AT TIME ZONE ?2, 'YYYY-MM-DD HH24:MI')",
                 basicTypeRegistry.resolve(StandardBasicTypes.STRING));
         functionRegistry.registerPattern("FORMAT_DATE_AS_YEAR_MONTH_DAY_HOUR",
-                "DATE_FORMAT(CONVERT_TZ(?1, '+00:00', ?2), '%Y-%m-%d %H')",
+                "to_char(?1 AT TIME ZONE ?2, 'YYYY-MM-DD HH24')",
                 basicTypeRegistry.resolve(StandardBasicTypes.STRING));
         functionRegistry.registerPattern("FORMAT_DATE_AS_YEAR_MONTH_DAY",
-                "DATE_FORMAT(CONVERT_TZ(?1, '+00:00', ?2), '%Y-%m-%d')",
+                "to_char(?1 AT TIME ZONE ?2, 'YYYY-MM-DD')",
                 basicTypeRegistry.resolve(StandardBasicTypes.STRING));
         functionRegistry.registerPattern("FORMAT_DATE_AS_YEAR_MONTH",
-                "DATE_FORMAT(CONVERT_TZ(?1, '+00:00', ?2), '%Y-%m')",
+                "to_char(?1 AT TIME ZONE ?2, 'YYYY-MM')",
                 basicTypeRegistry.resolve(StandardBasicTypes.STRING));
-        functionRegistry.registerPattern("FORMAT_DATE_AS_YEAR", "DATE_FORMAT(CONVERT_TZ(?1, '+00:00', ?2), '%Y')",
+        functionRegistry.registerPattern("FORMAT_DATE_AS_YEAR", "to_char(?1 AT TIME ZONE ?2, 'YYYY')",
                 basicTypeRegistry.resolve(StandardBasicTypes.STRING));
-        functionRegistry.registerPattern("CONVERT_TO_BIG_DECIMAL", "CAST(?1 AS DECIMAL(65,4))",
+        functionRegistry.registerPattern("CONVERT_TO_BIG_DECIMAL", "CAST(?1 AS DECIMAL)",
                 basicTypeRegistry.resolve(StandardBasicTypes.BIG_DECIMAL));
-        functionRegistry.registerPattern("CONVERT_TO_STRING", "CAST(?1 AS NCHAR)",
+        functionRegistry.registerPattern("CONVERT_TO_STRING", "CAST(?1 AS TEXT)",
                 basicTypeRegistry.resolve(StandardBasicTypes.STRING));
         functionRegistry.registerPattern("IS_NOT_DELETED_OF_ORGANIZE",
                 isNotDeleted("organize_entity"),
@@ -77,34 +70,34 @@ public class CustomMySQLDialect extends MySQLDialect {
         var getDescendantCountBuilder = new StringBuilder();
         getDescendantCountBuilder.append("(");
         getDescendantCountBuilder.append(" ");
-        getDescendantCountBuilder.append("WITH RECURSIVE `cte` AS (");
+        getDescendantCountBuilder.append("WITH RECURSIVE cte AS (");
         getDescendantCountBuilder.append(" ");
-        getDescendantCountBuilder.append("SELECT `id`, `parent_id`");
+        getDescendantCountBuilder.append("SELECT id, parent_id");
         getDescendantCountBuilder.append(" ");
-        getDescendantCountBuilder.append("FROM `" + tableName + "`");
+        getDescendantCountBuilder.append("FROM " + tableName + "");
         getDescendantCountBuilder.append(" ");
-        getDescendantCountBuilder.append("WHERE `id` = ?1");
+        getDescendantCountBuilder.append("WHERE id = ?1");
         getDescendantCountBuilder.append(" ");
         getDescendantCountBuilder.append("UNION ALL");
         getDescendantCountBuilder.append(" ");
         getDescendantCountBuilder
-                .append("SELECT `" + tmpTableNameAlias + "`.`id`, `" + tmpTableNameAlias + "`.`parent_id`");
+                .append("SELECT " + tmpTableNameAlias + ".id, " + tmpTableNameAlias + ".parent_id");
         getDescendantCountBuilder.append(" ");
-        getDescendantCountBuilder.append("FROM `cte` INNER JOIN `" + tableName + "` `" + tmpTableNameAlias + "`");
+        getDescendantCountBuilder.append("FROM cte INNER JOIN " + tableName + " " + tmpTableNameAlias + "");
         getDescendantCountBuilder.append(" ");
-        getDescendantCountBuilder.append("ON `cte`.`parent_id` = `" + tmpTableNameAlias + "`.`id`");
+        getDescendantCountBuilder.append("ON cte.parent_id = " + tmpTableNameAlias + ".id");
         getDescendantCountBuilder.append(" ");
         for (var condition : conditions) {
-            if (!condition.startsWith("`")) {
-                throw new RuntimeException("condition must start with \"`\"");
+            if (!condition.startsWith("")) {
+                throw new RuntimeException("condition must start with \"\"");
             }
-            getDescendantCountBuilder.append("AND `" + tmpTableNameAlias + "`." + condition + " ");
+            getDescendantCountBuilder.append("AND " + tmpTableNameAlias + "." + condition + " ");
         }
         getDescendantCountBuilder.append(")");
         getDescendantCountBuilder.append(" ");
-        getDescendantCountBuilder.append("SELECT COUNT(*) as total_record FROM `cte`");
+        getDescendantCountBuilder.append("SELECT COUNT(*) as total_record FROM cte");
         getDescendantCountBuilder.append(" ");
-        getDescendantCountBuilder.append("WHERE `cte`.`id` != ?1");
+        getDescendantCountBuilder.append("WHERE cte.id != ?1");
         getDescendantCountBuilder.append(" ");
         getDescendantCountBuilder.append(")");
         return getDescendantCountBuilder.toString();
@@ -115,37 +108,37 @@ public class CustomMySQLDialect extends MySQLDialect {
         var getDescendantCountBuilder = new StringBuilder();
         getDescendantCountBuilder.append("(");
         getDescendantCountBuilder.append(" ");
-        getDescendantCountBuilder.append("WITH RECURSIVE `cte` AS (");
+        getDescendantCountBuilder.append("WITH RECURSIVE cte AS (");
         getDescendantCountBuilder.append(" ");
-        getDescendantCountBuilder.append("SELECT `id`");
+        getDescendantCountBuilder.append("SELECT id");
         getDescendantCountBuilder.append(" ");
-        getDescendantCountBuilder.append("FROM `" + tableName + "`");
+        getDescendantCountBuilder.append("FROM " + tableName + "");
         getDescendantCountBuilder.append(" ");
-        getDescendantCountBuilder.append("WHERE `id` = ?1");
+        getDescendantCountBuilder.append("WHERE id = ?1");
         getDescendantCountBuilder.append(" ");
         getDescendantCountBuilder.append("UNION ALL");
         getDescendantCountBuilder.append(" ");
-        getDescendantCountBuilder.append("SELECT `" + tmpTableNameAlias + "`.`id`");
+        getDescendantCountBuilder.append("SELECT " + tmpTableNameAlias + ".id");
         getDescendantCountBuilder.append(" ");
-        getDescendantCountBuilder.append("FROM `cte` INNER JOIN `" + tableName + "` `" + tmpTableNameAlias + "`");
+        getDescendantCountBuilder.append("FROM cte INNER JOIN " + tableName + " " + tmpTableNameAlias + "");
         getDescendantCountBuilder.append(" ");
-        getDescendantCountBuilder.append("ON `cte`.`id` = `" + tmpTableNameAlias + "`.`parent_id`");
+        getDescendantCountBuilder.append("ON cte.id = " + tmpTableNameAlias + ".parent_id");
         getDescendantCountBuilder.append(" ");
         getDescendantCountBuilder.append("AND");
         getDescendantCountBuilder.append(" ");
-        getDescendantCountBuilder.append("`" + tmpTableNameAlias + "`.`is_deleted` = false");
+        getDescendantCountBuilder.append(tmpTableNameAlias + ".is_deleted = false");
         getDescendantCountBuilder.append(" ");
         for (var condition : conditions) {
-            if (!condition.startsWith("`")) {
-                throw new RuntimeException("condition must start with \"`\"");
+            if (!condition.startsWith("")) {
+                throw new RuntimeException("condition must start with \"\"");
             }
-            getDescendantCountBuilder.append("AND `" + tmpTableNameAlias + "`." + condition + " ");
+            getDescendantCountBuilder.append("AND " + tmpTableNameAlias + "." + condition + " ");
         }
         getDescendantCountBuilder.append(")");
         getDescendantCountBuilder.append(" ");
-        getDescendantCountBuilder.append("SELECT COUNT(*) as total_record FROM `cte`");
+        getDescendantCountBuilder.append("SELECT COUNT(*) as total_record FROM cte");
         getDescendantCountBuilder.append(" ");
-        getDescendantCountBuilder.append("WHERE `cte`.`id` != ?1");
+        getDescendantCountBuilder.append("WHERE cte.id != ?1");
         getDescendantCountBuilder.append(" ");
         getDescendantCountBuilder.append(")");
         return getDescendantCountBuilder.toString();
@@ -158,27 +151,27 @@ public class CustomMySQLDialect extends MySQLDialect {
         isChildBuilder.append(" ");
         isChildBuilder.append("(");
         isChildBuilder.append(" ");
-        isChildBuilder.append("WITH RECURSIVE `cte` AS (");
+        isChildBuilder.append("WITH RECURSIVE cte AS (");
         isChildBuilder.append(" ");
-        isChildBuilder.append("SELECT `id`");
+        isChildBuilder.append("SELECT id");
         isChildBuilder.append(" ");
-        isChildBuilder.append("FROM `" + tableName + "`");
+        isChildBuilder.append("FROM " + tableName + "");
         isChildBuilder.append(" ");
-        isChildBuilder.append("WHERE `id` = ?1");
+        isChildBuilder.append("WHERE id = ?1");
         isChildBuilder.append(" ");
         isChildBuilder.append("UNION ALL");
         isChildBuilder.append(" ");
-        isChildBuilder.append("SELECT `" + tmpTableNameAlias + "`.`parent_id` as `id`");
+        isChildBuilder.append("SELECT " + tmpTableNameAlias + ".parent_id as id");
         isChildBuilder.append(" ");
-        isChildBuilder.append("FROM `cte` INNER JOIN `" + tableName + "` `" + tmpTableNameAlias + "`");
+        isChildBuilder.append("FROM cte INNER JOIN " + tableName + " " + tmpTableNameAlias + "");
         isChildBuilder.append(" ");
-        isChildBuilder.append("ON `cte`.`id` = `" + tmpTableNameAlias + "`.`id`");
+        isChildBuilder.append("ON cte.id = " + tmpTableNameAlias + ".id");
         isChildBuilder.append(" ");
         isChildBuilder.append(")");
         isChildBuilder.append(" ");
-        isChildBuilder.append("SELECT * FROM `cte`");
+        isChildBuilder.append("SELECT * FROM cte");
         isChildBuilder.append(" ");
-        isChildBuilder.append("WHERE `cte`.`id` = ?2");
+        isChildBuilder.append("WHERE cte.id = ?2");
         isChildBuilder.append(" ");
         isChildBuilder.append(")");
         return isChildBuilder.toString();
@@ -191,62 +184,58 @@ public class CustomMySQLDialect extends MySQLDialect {
         isNotDeletedBuilder.append(" ");
         isNotDeletedBuilder.append("(");
         isNotDeletedBuilder.append(" ");
-        isNotDeletedBuilder.append("WITH RECURSIVE `cte` AS (");
+        isNotDeletedBuilder.append("WITH RECURSIVE cte AS (");
         isNotDeletedBuilder.append(" ");
-        isNotDeletedBuilder.append("SELECT `id`");
+        isNotDeletedBuilder.append("SELECT id");
         isNotDeletedBuilder.append(" ");
-        isNotDeletedBuilder.append("FROM `" + tableName + "`");
+        isNotDeletedBuilder.append("FROM " + tableName + "");
         isNotDeletedBuilder.append(" ");
-        isNotDeletedBuilder.append("WHERE `id` = ?1");
+        isNotDeletedBuilder.append("WHERE id = ?1");
         isNotDeletedBuilder.append(" ");
         isNotDeletedBuilder.append("AND");
         isNotDeletedBuilder.append(" ");
-        isNotDeletedBuilder.append("`is_deleted` = false");
+        isNotDeletedBuilder.append("is_deleted = false");
         isNotDeletedBuilder.append(" ");
         isNotDeletedBuilder.append("UNION ALL");
         isNotDeletedBuilder.append(" ");
-        isNotDeletedBuilder.append("SELECT IFNULL(`" + tmpTableNameAlias + "`.`parent_id`, 'NULL') as `id`");
+        isNotDeletedBuilder.append("SELECT IFNULL(" + tmpTableNameAlias + ".parent_id, 'NULL') as id");
         isNotDeletedBuilder.append(" ");
-        isNotDeletedBuilder.append("FROM `cte` INNER JOIN `" + tableName + "` `" + tmpTableNameAlias + "`");
+        isNotDeletedBuilder.append("FROM cte INNER JOIN " + tableName + " " + tmpTableNameAlias + "");
         isNotDeletedBuilder.append(" ");
-        isNotDeletedBuilder.append("ON `cte`.`id` = `" + tmpTableNameAlias + "`.`id`");
+        isNotDeletedBuilder.append("ON cte.id = " + tmpTableNameAlias + ".id");
         isNotDeletedBuilder.append(" ");
         isNotDeletedBuilder.append("AND");
         isNotDeletedBuilder.append(" ");
-        isNotDeletedBuilder.append("`" + tmpTableNameAlias + "`.`is_deleted` = false");
+        isNotDeletedBuilder.append("" + tmpTableNameAlias + ".is_deleted = false");
         isNotDeletedBuilder.append(" ");
         isNotDeletedBuilder.append(")");
         isNotDeletedBuilder.append(" ");
-        isNotDeletedBuilder.append("SELECT * FROM `cte`");
+        isNotDeletedBuilder.append("SELECT * FROM cte");
         isNotDeletedBuilder.append(" ");
-        isNotDeletedBuilder.append("WHERE `cte`.`id` = 'NULL'");
+        isNotDeletedBuilder.append("WHERE cte.id = 'NULL'");
         isNotDeletedBuilder.append(" ");
         isNotDeletedBuilder.append(")");
         return isNotDeletedBuilder.toString();
     }
 
-    public CustomMySQLDialect() {
+    public CustomCockroachdbDialect() {
         super();
     }
 
-    public CustomMySQLDialect(DatabaseVersion version) {
+    public CustomCockroachdbDialect(DialectResolutionInfo info) {
+        super(info);
+    }
+
+    public CustomCockroachdbDialect(DialectResolutionInfo info, String versionString) {
+        super(info, versionString);
+    }
+
+    public CustomCockroachdbDialect(DatabaseVersion version) {
         super(version);
     }
 
-    public CustomMySQLDialect(DatabaseVersion version, int bytesPerCharacter) {
-        super(version, bytesPerCharacter);
-    }
-
-    public CustomMySQLDialect(DatabaseVersion version, MySQLServerConfiguration serverConfiguration) {
-        super(version, serverConfiguration);
-    }
-
-    public CustomMySQLDialect(DatabaseVersion version, int bytesPerCharacter, boolean noBackslashEscapes) {
-        super(version, bytesPerCharacter, noBackslashEscapes);
-    }
-
-    public CustomMySQLDialect(DialectResolutionInfo info) {
-        super(info);
+    public CustomCockroachdbDialect(DatabaseVersion version, PostgreSQLDriverKind driverKind) {
+        super(version, driverKind);
     }
 
 }
