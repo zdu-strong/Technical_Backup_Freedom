9636a98ece82392ca84aea7badaba4e4542fa559
zdu <zdu.strong@gmail.com>
2024-01-26 22:29:07 +0900

    #: Change to a new id of EncryptDecryptEntity

--- springboot/src/main/java/com/springboot/project/enumeration/EncryptDecryptEnum.java
+++ springboot/src/main/java/com/springboot/project/enumeration/EncryptDecryptEnum.java
@@ -2,6 +2,6 @@ package com.springboot.project.enumeration;
 
 public class EncryptDecryptEnum {
 
-    public final static String KEY_ID = "a6348051-646d-4f37-a68a-75a5a28a1d67";
+    public final static String KEY_ID = "1eebc4ea-34b3-64fd-bdc4-47396785f5dd";
 
 }
