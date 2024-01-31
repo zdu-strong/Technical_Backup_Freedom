9c6379986482da2ec06e523ba7940979ad0d5115
zdu <zdu.strong@gmail.com>
2024-01-31 22:01:29 +0900

    #: Optimize code

--- springboot/src/main/java/com/springboot/project/properties/StorageRootPathProperties.java
+++ springboot/src/main/java/com/springboot/project/properties/StorageRootPathProperties.java
@@ -1,11 +1,11 @@
 package com.springboot.project.properties;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import lombok.Getter;
 import lombok.Setter;
 
-@Configuration
+@Component
 @Getter
 @Setter
 public class StorageRootPathProperties {
