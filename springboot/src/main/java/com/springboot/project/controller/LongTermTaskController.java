b7ab919a55ae5804f9cdbfdbb5e91372ae4c8c64
zdu <zdu.strong@gmail.com>
2024-02-03 12:18:04 +0900

    #: Add enum that wait duration of LongTermTaskController

--- springboot/src/main/java/com/springboot/project/controller/LongTermTaskController.java
+++ springboot/src/main/java/com/springboot/project/controller/LongTermTaskController.java
@@ -1,19 +1,25 @@
 package com.springboot.project.controller;
 
-import java.time.Duration;
 import java.util.Calendar;
 import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springboot.project.common.baseController.BaseController;
+import com.springboot.project.enumerate.LongTermTaskTempWaitDurationEnum;
 import com.springboot.project.model.LongTermTaskModel;
 
 @RestController
 public class LongTermTaskController extends BaseController {
-    private Duration tempWaitDuration = Duration.ofSeconds(30);
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     /**
      * Because some requests take a long time to execute, so provide this
@@ -24,26 +30,32 @@ public class LongTermTaskController extends BaseController {
      * @param id
      * @return
      * @throws InterruptedException
+     * @throws JsonProcessingException
+     * @throws JsonMappingException
      */
     @GetMapping("/long_term_task")
-    public ResponseEntity<?> getLongTermTask(@RequestParam String id) throws InterruptedException {
+    public ResponseEntity<?> getLongTermTask(@RequestParam String id)
+            throws InterruptedException, JsonMappingException, JsonProcessingException {
 
         this.longTermTaskService
                 .checkIsExistLongTermTaskById(this.encryptDecryptService.decryptByAES(id));
 
         Calendar calendarOfWait = Calendar.getInstance();
         calendarOfWait.setTime(new Date());
-        calendarOfWait.add(Calendar.MILLISECOND, Long.valueOf(this.tempWaitDuration.toMillis()).intValue());
+        calendarOfWait.add(Calendar.MILLISECOND,
+                Long.valueOf(LongTermTaskTempWaitDurationEnum.TEMP_WAIT_DURATION.toMillis()).intValue());
         Date expireDate = calendarOfWait.getTime();
 
         while (true) {
             var response = this.longTermTaskService
                     .getLongTermTask(this.encryptDecryptService.decryptByAES(id));
             if (response.getBody() instanceof LongTermTaskModel) {
-                @SuppressWarnings("unchecked")
-                var longTermTaskResult = (ResponseEntity<LongTermTaskModel<?>>) response;
-                if (longTermTaskResult.getBody().getIsDone() || !new Date().before(expireDate)) {
-                    return longTermTaskResult;
+                var longTermTaskResult = this.objectMapper.readValue(
+                        this.objectMapper.writeValueAsString(response.getBody()),
+                        new TypeReference<LongTermTaskModel<Object>>() {
+                        });
+                if (longTermTaskResult.getIsDone() || !new Date().before(expireDate)) {
+                    return response;
                 } else {
                     Thread.sleep(1);
                 }
---
+++ springboot/src/main/java/com/springboot/project/enumerate/LongTermTaskTempWaitDurationEnum.java
@@ -0,0 +1,10 @@
+package com.springboot.project.enumerate;
+
+import java.time.Duration;
+
+public class LongTermTaskTempWaitDurationEnum {
+    /**
+     * temp wait duration
+     */
+    public final static Duration TEMP_WAIT_DURATION = Duration.ofSeconds(30);
+}
