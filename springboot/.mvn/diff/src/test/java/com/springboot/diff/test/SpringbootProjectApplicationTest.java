a54a3c93e7ebef5a63e1a3428ec4377691688b70
zdu <zdu.strong@gmail.com>
2024-02-23 17:17:00 +0900

    #: Add unit tests

---
+++ springboot/.mvn/diff/src/test/java/com/springboot/diff/test/SpringbootProjectApplicationTest.java
@@ -0,0 +1,14 @@
+package com.springboot.diff.test;
+
+import org.junit.jupiter.api.Test;
+import com.springboot.diff.SpringbootProjectApplication;
+import com.springboot.diff.test.common.BaseTest.BaseTest;
+
+public class SpringbootProjectApplicationTest extends BaseTest {
+
+    @Test
+    public void test() throws Throwable {
+        SpringbootProjectApplication.main(new String[] {});
+    }
+
+}
